8951f00b23b803cc8b5d6428f065c59754f42b6c
Bastian Krol <bastian.krol@web.de>
2016-10-18 15:52:44 +0200

    separate types for configuration and executable parameters

--- programs/MooSQLite.hs
+++ programs/MooSQLite.hs
@@ -14,21 +14,16 @@ import Moo.Main
 
 main :: IO ()
 main = do
-  -- make sure the configuration validation does not complain about a missing
-  -- db type
-  setEnv "DBM_DATABASE_TYPE" "sqlite3"
-
   args <- getArgs
   (_, opts, _) <- procArgs args
   conf <-
     loadConfiguration $ _configFilePath opts
   case conf of
-      Left e -> putStrLn e >> exitFailure
-      Right preliminaryConf -> do
-        let Left preliminaryDatabaseConf =  _backendOrConf preliminaryConf
-            connectionString = _connectionString preliminaryDatabaseConf
-        connection <- connectSqlite3 connectionString
-        let backend = hdbcBackend connection
-            finalConf = preliminaryConf { _backendOrConf = Right backend }
-        mainWithConf args finalConf
+    Left e -> putStrLn e >> exitFailure
+    Right conf -> do
+      let connectionString = _connectionString conf
+      connection <- connectSqlite3 connectionString
+      let backend = hdbcBackend connection
+          parameters = makeParameters conf backend
+      mainWithParameters args parameters
 
